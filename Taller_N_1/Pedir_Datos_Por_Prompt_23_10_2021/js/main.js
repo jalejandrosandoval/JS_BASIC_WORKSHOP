320a604eaba61bd91f189eee59dc7085f7d9bc72
jalejandrosandoval <jsandoval5@udi.edu.co>
2021-10-23 12:18:10 -0500

    Restore Master

--- Taller_N_1/Pedir_Datos_Por_Prompt_23_10_2021/js/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var _data = prompt("Ingresar un número:");
-if(_data === '' || _data === null)
-    alert("Ingrese un número válido...");
-else
-    console.log(`Número Ingresado: ${_data}`);
