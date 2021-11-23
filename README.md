f2a5c62965d109994151f920021a741b7bc4eb86
jalejandrosandoval <jsandoval5@udi.edu.co>
2021-11-23 18:46:51 -0500

    Ajuste en el Readme.

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Jhon Alejandro Sandoval Miranda...
 * Taller_N_1: Taller # 1 de JS.
 * Taller_N_2 06112021: Taller # 2 de JS => Operadores y Otros.
 * Taller_N_3 09112021: Taller # 3 de JS => Ciclos.
+* Taller_N_4 20112021: Taller # 4 de JS => Arrays.
--- Taller_N_1/Forma_Incluir_Javascript_02_23_10_2021/js/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-//Console Log
-console.log(`%cConsole: Conexión con el archivo JavaScript Exitoso.`,
-            `font-family; color:white; 
-             background:#162c40;
-             padding: 3px;
-             border-radius: 5px;
-             font-weight: 700`);
